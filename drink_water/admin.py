f50b1b961b99b0225b166f4015f30d6071e3bee7
Bruno R. Scherer <brunoricardojava@gmail.com>
2024-07-28 22:02:37 -0300

    Add admin of models django.

--- drink_water/admin.py
+++ drink_water/admin.py
@@ -1,1 +1,13 @@
-# Register your models here.
+from django.contrib import admin
+
+from drink_water.models import User, UserAction
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["name", "weight", "created_at"]
+    search_fields = ["name"]
+
+@admin.register(UserAction)
+class UserActionAdmin(admin.ModelAdmin):
+    list_display = ["user", "action", "quantity", "created_at"]
+    search_fields = ["user__name"]
--- drink_water/models/user_model.py
+++ drink_water/models/user_model.py
@@ -6,3 +6,6 @@ from .base_model import BaseModel
 class User(BaseModel):
     name = models.CharField(max_length=50, db_index=True)
     weight = models.FloatField()
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.id}"
